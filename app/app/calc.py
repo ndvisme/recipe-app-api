d7230e8632a2218ae95c2d6f38489689e3a53e72
nadav-sgh <nspier@sightd.io>
2025-05-02 09:28:12 +0300

    Add 'wait_for_db' cmd implementation.

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,5 +3,6 @@
 def add(x: int, y: int) -> int:
     return x + y
 
+
 def subtract(x: int, y: int) -> int:
     return x - y
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -5,6 +5,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
 
@@ -13,7 +14,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -23,4 +24,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
