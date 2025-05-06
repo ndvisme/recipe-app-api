a4ff2b05b6f02369f2181bfd905f802f57a6b481
nadav-sgh <nspier@sightd.io>
2025-05-06 09:35:49 +0300

    Require email input.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,3 +29,10 @@ class ModelTests(TestCase):
                 password='sample123'
             )
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email='',
+                password='pass123'
+            )
